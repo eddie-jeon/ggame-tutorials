7f1d467263113f8dbb3375e3e36f9af82ff8eeed
eddie-jeon <zedwardjeone@gmail.com>
2016-07-20 14:38:45 -0400

    Updated from Brython Server: 7/20/2016 2:38:44 PM

--- tutorial3.py
+++ tutorial3.py
@@ -14,7 +14,7 @@ bg = Sprite(bg_asset, (0,0))
 # A ball! This is already in the ggame-tutorials repository
 ball_asset = ImageAsset("images/orb-150545_640.png")
 ball = Sprite(ball_asset, (0, 0))
-ball.fxcenter = 0.5
+ball.fxcenter = 0.6
 ball.fycenter = 0.6
 # Original image is too big. Scale it to 1/10 its original size
 ball.scale = 0.1
@@ -30,7 +30,7 @@ pop = Sound(pop_asset)
 
 def reverse(b):
     b.dir *= -1
-'''    
+
 # Set up function for handling screen refresh
 def step():
     if ball.go:
@@ -38,7 +38,7 @@ def step():
         if ball.x + ball.width > SCREEN_WIDTH or ball.x < 0:
             ball.x -= ball.dir
             reverse(ball)    
-'''
+
 # Handle the W key
 def wKey(event):
     ball.y -= 5
