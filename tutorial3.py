a2843b177469bd833a1fa6af6ec12a82ac2e5ab1
eddie-jeon <zedwardjeone@gmail.com>
2016-07-20 15:30:15 -0400

    Updated from Brython Server: 7/20/2016 3:30:15 PM

--- tutorial3.py
+++ tutorial3.py
@@ -2,22 +2,13 @@
 from ggame import App, RectangleAsset, ImageAsset, SoundAsset
 from ggame import LineStyle, Color, Sprite, Sound
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-
-green = Color(0x00ff00, 1)
-black = Color(0, 1)
-noline = LineStyle(0, black)
-bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
-bg = Sprite(bg_asset, (0,0))
-
 # A ball! This is already in the ggame-tutorials repository
 ball_asset = ImageAsset("images/orb-150545_640.png")
 ball = Sprite(ball_asset, (0, 0))
-ball.fxcenter = 0.6
+ball.fxcenter = 0.7
 ball.fycenter = 0.6
 # Original image is too big. Scale it to 1/10 its original size
-ball.scale = 0.1
+ball.scale = 0.05
 # custom attributes
 ball.dir = 1
 ball.go = True
